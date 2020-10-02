75c30d4e3e1d061c1319970646b514ef615ed3d7
Gina Sprint <sprint@gonzaga.edu>
2020-10-02 09:48:42 -0700

    Initial Commit

---
+++ MoreSwiftFunS2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MoreSwiftFunS2
+//
+//  Created by Gina Sprint on 10/2/20.
+//  Copyright © 2020 Gina Sprint. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
